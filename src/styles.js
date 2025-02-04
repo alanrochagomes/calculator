a49ebf41bb76954f8b4f1311d766f262137a8360
Alan Rocha <alanrochaarg2001@gmail.com>
2025-02-04 10:03:03 -0300

    chore: creating the main buttons

--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import App from "./App";
 
 export const Container = styled.div`
     width: 100%;
-    height: 100%;
+    height: 100vh;
     background-color: #CACACA;
 
     display: flex;
@@ -13,6 +12,18 @@ export const Container = styled.div`
 
 export const Content = styled.div`
     background-color: #FFFFFF;
-    width: 80%;
-    min-height: 350px;
+    width: 50%;
+`
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
