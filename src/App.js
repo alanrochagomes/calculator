32e5a65d543e02fac0483fc72168e9c668dd3f1e
Alan Rocha <alanrochaarg2001@gmail.com>
2025-02-04 11:56:16 -0300

    feat: adding subtraction function

--- src/App.js
+++ src/App.js
@@ -32,11 +32,26 @@ const App = () => {
     }
   };
 
+  const handleMinusNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    } else {
+      const sum = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
   const handleEquals = () => {
     if (firstNumber != '0' && operation != '' && currentNumber != '0') {
      switch(operation) {
        case '+':
          handleSumNumbers();
+         break;
+       case '-':
+         handleMinusNumbers();
          break;
          default:
          break;
@@ -51,15 +66,15 @@ const App = () => {
         <Row>
           <Button label="x" />
           <Button label="/" />
-          <Button label="C" onClick={handleOnClear}/>
-          <Button label="X" />
+          <Button label="c" onClick={handleOnClear}/>
+          <Button label="." />
         </Row>
 
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-"/>
+          <Button label="-" onClick={handleMinusNumbers}/>
         </Row>
 
         <Row>
